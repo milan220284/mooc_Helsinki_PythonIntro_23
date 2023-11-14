1f93a1664fe11c30392bf1c413bcbbda6b92f041
Milan <milan220284@hotmail.com>
2023-11-14 22:53:42 +0100

    update 07 part

--- part07-11_screen_time/src/screen_time.py
+++ part07-11_screen_time/src/screen_time.py
@@ -1,1 +1,17 @@
 # Write your solution here
+import json
+
+def print_persons(filename: str):
+
+    with open(filename) as my_file:
+        data = my_file.read()
+
+    database = json.loads(data)
+
+    for person in database:
+        hobbies = '('
+        for hoby in person["hobbies"]:
+            hobbies += hoby
+            hobbies += ','
+        hobbies = hobbies[:-1] + ')' 
+        print(f'{person['name']} {person['age']} years ' + hobbies )
--- part07-13_course_statistics/src/course_statistics.py
+++ part07-13_course_statistics/src/course_statistics.py
@@ -1,1 +1,15 @@
 # Write your solution here
+import urllib.request
+
+def retrieve_all():
+    my_request = urllib.request.urlopen("https://studies.cs.helsinki.fi/stats-mock/api/courses")
+    database = my_request.read()
+    active = []
+
+
+
+    print(database[0])
+
+
+
+retrieve_all()
