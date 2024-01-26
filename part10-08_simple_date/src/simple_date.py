e3d2032c19fe0e22e87efcb09eefa43294e7ffaa
Milan <milan220284@hotmail.com>
2024-01-26 20:59:38 +0100

    update 10 with overload operators

--- part10-08_simple_date/src/simple_date.py
+++ part10-08_simple_date/src/simple_date.py
@@ -1,2 +1,65 @@
 # WRITE YOUR SOLUTION HERE:
 
+class SimpleDate:
+    def __init__(self, day, month, year) -> None:
+        self.day = day
+        self.month = month
+        self.year = year
+    
+    def __eq__(self, another) -> bool:
+        if self.day == another.day and self.month == another.month and self.year == another.year:
+            return True
+        else:
+            return False  
+        
+    def __lt__(self, another) -> bool:
+        if self.year < another.year:
+            return True
+        elif self.year == another.year:
+            if self.month < another.month:
+                return True
+            elif self.month == another.month:
+                if self.day < another.day:
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False   
+
+    def __gt__(self, another) -> bool:
+        if self.year > another.year:
+            return True
+        elif self.year == another.year:
+            if self.month > another.month:
+                return True
+            elif self.month == another.month:
+                if self.day > another.day:
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False             
+    
+    def __ne__(self, another) -> bool:
+        if self.__eq__(another):
+            return False
+        else:
+            return True 
+    
+    def __add__(self, days) -> bool:
+        tmp = self.year*12*30 + self.month*30 + self.day
+        tmp_year = (tmp+days)//360
+        tmp_month = (tmp+days - tmp_year*360)//30
+        tmp_day = tmp+days - tmp_year*360 - tmp_month*30
+        return SimpleDate(tmp_day, tmp_month, tmp_year)
+        
+    def __str__(self) -> str:
+        return f"{self.day}.{self.month}.{self.year}"
+
+    def __sub__(self, another) -> bool:
+        return abs(another.year*360+another.month*30+another.day - self.year*360-self.month*30-self.day)
+
--- part10-09_iterable_shopping_list/src/iterable_shopping_list.py
+++ part10-09_iterable_shopping_list/src/iterable_shopping_list.py
@@ -14,3 +14,18 @@ class ShoppingList:
 
     def number(self, n: int):
         return self.products[n - 1][1]
+    
+    def __iter__(self):
+        self.n = 0
+        # the method returns a reference to the object itself as 
+        # the iterator is implemented within the same class definition
+        return self
+
+    def __next__(self):
+        if self.n < len(self.products):
+            item = self.products[self.n]
+            self.n += 1
+            return item
+        else:
+            raise StopIteration
+
